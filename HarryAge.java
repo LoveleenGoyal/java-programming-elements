3e4cc81b5b5d9d91e6736e0b07997626ce2d1900
LoveleenGoyal <loveleengoyal26@gmail.com>
2025-02-28 10:26:06 +0530

    [Loveleen] feat: Finding the age of Harry

---
+++ HarryAge.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+
+class HarryAge{
+	public static void main(String[] args){
+		int birthYear = 2000;
+		int currYear = 2024;
+		int presentAge = currYear - birthYear;
+		System.out.print("Harry's age in 2024 is : " + presentAge);
+	}
+}
