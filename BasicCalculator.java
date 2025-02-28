c95748252a76ff8c9fc56364716536e8be81a050
LoveleenGoyal <loveleengoyal26@gmail.com>
2025-02-28 19:16:58 +0530

    [Loveleen] feat: Created a basic calculator

--- BasicCalculator.java
+++ BasicCalculator.java
@@ -1,25 +1,27 @@
-import java.util.Scanner;
+import java.util.Scanner; // Import Scanner for user input
 
-class BasicCalculator{
-	public static void main(String[] args){
-		Scanner sc = new Scanner(System.in);
-		
-		System.out.print("Enter number 1: ");
-		float number1 = sc.nextInt();
-		
-		System.out.print("\nEnter number 2: ");
-		float number2 = sc.nextInt();
-		
-		float add = number1 + number2;
-		
-		float subtract = number1 - number2;
-		
-		float multiply = number1 * number2;
-		
-		float divisin = number1 / number2;
-		
-		System.out.print("\nThe addition, subtraction, multiplication, and division value of 2 numbers " + number1 + " and " + number2 + " is " + add + ", " + subtract + ", " + multiply + ", and " + divisin);
-	}
+// Class to create a basic calculator
+class BasicCalculator {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in); 
+
+        // Asking user to enter first number
+        System.out.print("Enter number 1: ");
+        float number1 = sc.nextFloat();
+
+        // Asking user to enter second number
+        System.out.print("Enter number 2: ");
+        float number2 = sc.nextFloat();
+
+        // Performing arithmetic operations
+        float addition = number1 + number2; 
+        float subtract = number1 - number2;
+        float multiply = number1 * number2;
+        float division = number1 / number2; 
+
+        // Displaying results
+        System.out.println("\nThe addition, subtraction, multiplication, and division value of 2 numbers " 
+            + number1 + " and " + number2 + " is " 
+            + addition + ", " + subtract + ", " + multiply + ", and " + division);
+    }
 }
-	
-
